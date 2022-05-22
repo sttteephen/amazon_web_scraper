c2ded96593160126bca850e36c0e71c643c3812c
Stephen Messer <40314216@live.napier.ac.uk>
2022-05-22 21:48:56 +0100

    does sentiment analysis on reviews

---
+++ review_sentiment.py
@@ -0,0 +1,17 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+import csv
+
+sia = SentimentIntensityAnalyzer()
+with open("reviews.csv") as csv_file:
+
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count = 0
+
+    for row in csv_reader:
+
+        if line_count == 0:
+            pass
+        else:
+            print(row[2], " ", sia.polarity_scores(row[3]))
+
+        line_count += 1
