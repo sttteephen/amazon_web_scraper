7cb03a6c26498d438a8d09223eeda9f8773d0225
Stephen Messer <40314216@live.napier.ac.uk>
2022-05-25 21:42:53 +0100

    graphs average sentiment score for each rating

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -25,12 +25,16 @@ class AmazonReview:
 def get_page_html(url):
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "DNT": "1",
-        "Connection": "close",
-        "Upgrade-Insecure-Requests": "1",
+        "dnt": "1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-user": "?1",
+        "sec-fetch-dest": "document",
+        "referer": "https://www.amazon.com/",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
     }
 
     # repeat request until it doesn't receive captcha page
--- graph_review.py
+++ graph_review.py
@@ -1,1 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
 
+sentiment_dict = {1: [], 2: [], 3: [], 4: [], 5: []}
+
+with open("reviews.csv", "r") as reviews_file:
+
+    csv_reader = csv.reader(reviews_file, delimiter=",")
+    next(csv_reader, None)
+
+    for row in csv_reader:
+        # print(row)
+
+        sentiment_dict[int(row[2])].append(float(row[4]))
+
+x_values = []
+y_values = []
+
+# iterate through dictionary in acending rating order
+for rating in sorted(sentiment_dict.keys()):
+
+    # if there is data for that rating append rating to x and average sentiment score to y
+    if len(sentiment_dict[rating]) > 0:
+        x_values.append(rating)
+        y_values.append(sum(sentiment_dict[rating]) / len(sentiment_dict[rating]))
+
+
+x = np.array(x_values)
+y = np.array(y_values)
+
+plt.bar(x, y)
+plt.show()
